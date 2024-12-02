a5dbfa76d00967a03b6914b9a8e072561f38595f
Wevton Junior <wevtonj@gmail.com>
2024-12-02 12:41:41 -0300

    Arrumando a main.

--- principal/main.cpp
+++ principal/main.cpp
@@ -8,5 +8,6 @@ int main(int argc, char * argv[]) {
     string nome = getNome(); // pegar o nome do usuário.
     introTela(nome); // Introduzir o usuário ao sistema.
     montarComputador(); // arrumar essa função ainda.
+    delayTexto("Obrigado por testar nossos serviços, Sr.(a) " + nome + "! Qualquer feedback é bem vindo!\n", 1);
     return 0;
 }
